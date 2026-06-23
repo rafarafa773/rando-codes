c07cbbf2880917bb5ed2da01910b202c5e3cf246
rafa <rafaelfag18@proton.me>
2026-06-22 23:00:26 -0300

    Add files via upload

---
+++ ok/wc.h
@@ -0,0 +1,12 @@
+??=define f(x) fprintf(stderr, "%s\n", x)
+??=define e(x) return x;
+??=define v void
+??=define it int
+??=define m main
+??=define ca char
+??=define w while
+??=define r fopen
+??=define t size_t
+??=define o fgetc
+??=define __ FILE
+??=define p	== '\n'
